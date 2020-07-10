21bab17eaf8fab960e75249427cb95b0e5b0d11d
Jarrell Jackson <jdjack3@gmail.com>
2020-07-10 18:42:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,11 +9,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  puts division = num1 / num2
+  division
 end
 
 def multiplication(num1, num2)
-
+  puts multiplication = num1 * num2
+  multiplication
 end
 
 def modulo(num1, num2)
